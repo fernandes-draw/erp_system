5be01e532360ab1cc47be7f064db79f5b3617c15
fernandes-draw <fernandes_draw@hotmail.com>
2026-03-13 15:08:24 -0300

    configurando models. forms e admin com CustomUser

--- core/settings.py
+++ core/settings.py
@@ -25,6 +25,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # My APPS
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -103,3 +106,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+AUTH_USER_MODEL = "accounts.CustomUser"
