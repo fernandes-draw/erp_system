b0ae2c0ded935881d688abbbcb3e4240715a5ec8
Everson Fernandes <fernandes_draw@hotmail.com>
2026-03-13 19:46:47 -0300

    Criar DashboardView

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,15 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from .forms import CustomUserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
 
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+
+class DashboardView(LoginRequiredMixin, TemplateView):
+    template_name = "accounts/dashboard.html"
