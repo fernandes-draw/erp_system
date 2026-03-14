e8db3e60acc776ddca4548245a9df7b8e6150aec
Everson Fernandes <fernandes_draw@hotmail.com>
2026-03-14 13:36:54 -0300

    Registrar a URL (accounts/urls.py)

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SignUpView, ProfileUpdateView, DashboardView
+from .views import SignUpView, ProfileUpdateView, DashboardView, UserListView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path("profile/edit/", ProfileUpdateView.as_view(), name="profile_edit"),
     path("dashboard/", DashboardView.as_view(), name="dashboard"),
+    path("users/", UserListView.as_view(), name="user_list"),
 ]
