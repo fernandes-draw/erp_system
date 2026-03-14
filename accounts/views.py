28cce00f7574ea0ebf82380ec7afa8887937493d
Everson Fernandes <fernandes_draw@hotmail.com>
2026-03-13 23:22:42 -0300

    Refatorando, separando os templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,20 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from .forms import CustomUserCreationForm
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 
-class SignUpView(CreateView):
+class SignUpView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+    
+    # Função que define QUEM  pode acessar essa página
+    def test_func(self):
+        
+        # Apenas usuários logados que sejam 'staff' (gerentes/admin)
+        return self.request.user.is_staff
 
 
 class DashboardView(LoginRequiredMixin, TemplateView):
