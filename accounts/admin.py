48bdeafb2a87ae0329be11e7b86ed61ea9f5e575
Everson Fernandes <fernandes_draw@hotmail.com>
2026-03-14 13:38:42 -0300

    Adicionando novos campos no admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,19 +1,30 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
-
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
+    form = CustomUserChangeForm  # Mantemos o seu form de edição
     model = CustomUser
+    
+    # Colunas que aparecem na lista de usuários
     list_display = [
-        "email",
         "username",
+        "email",
+        "cargo", # Novo
         "is_staff",
         "is_active",
     ]
 
+    # Campos que aparecem na tela de edição do usuário
+    fieldsets = UserAdmin.fieldsets + (
+        ('Informações Profissionais', {'fields': ('cargo', 'avatar')}),
+    )
+
+    # Campos que aparecem na tela de criação (pelo admin)
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Informações Profissionais', {'fields': ('cargo', 'avatar')}),
+    )
 
 admin.site.register(CustomUser, CustomUserAdmin)
