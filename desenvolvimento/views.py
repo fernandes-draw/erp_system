9a3ca4e1f35292369008767728bfe5b021b1951e
Everson Fernandes <fernandes_draw@hotmail.com>
2026-03-14 18:59:54 -0300

    View (desenvolvimento/views.py)

--- desenvolvimento/views.py
+++ desenvolvimento/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from .models import Amostra
+from .forms import AmostraForm
 
-# Create your views here.
+class AmostraCreateView(LoginRequiredMixin, CreateView):
+    model = Amostra
+    form_class = AmostraForm
+    template_name = 'desenvolvimento/amostra_form.html'
+    success_url = reverse_lazy('amostra_list')
+
+    def form_valid(self, form):
+        form.instance.cadastrado_por = self.request.user
+        return super().form_valid(form)
+
+class AmostraListView(LoginRequiredMixin, ListView):
+    model = Amostra
+    template_name = 'desenvolvimento/amostra_list.html'
+    context_object_name = 'amostras'
+    ordering = ['-data_recebimento']
