4cbedce391d977f8ecf7f6e10ef50439f80cbd2d
Everson Fernandes <fernandes_draw@hotmail.com>
2026-03-14 21:22:51 -0300

    Script de Atualização (projeto_kanban.html)

--- desenvolvimento/urls.py
+++ desenvolvimento/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('amostras/nova/', AmostraCreateView.as_view(), name='amostra_create'),
     path('amostras/iniciar-projeto/<int:amostra_id>/', iniciar_projeto, name='iniciar_projeto'),
     path('kanban/', ProjetoKanbanView.as_view(), name='projeto_kanban'),
+    path('kanban/update/', atualizar_status_projeto, name='atualizar_status_projeto'),
 ]
