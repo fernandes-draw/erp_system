15ba7b33cb0088f11a6858251d4139a75594873f
Everson Fernandes <fernandes_draw@hotmail.com>
2026-03-14 21:01:41 -0300

    Template do Kanban (templates/desenvolvimento/projeto_kanban.html)

--- desenvolvimento/urls.py
+++ desenvolvimento/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AmostraCreateView, AmostraListView, iniciar_projeto
+from .views import AmostraCreateView, AmostraListView, ProjetoKanbanView, iniciar_projeto
 
 urlpatterns = [
     path('amostras/', AmostraListView.as_view(), name='amostra_list'),
     path('amostras/nova/', AmostraCreateView.as_view(), name='amostra_create'),
     path('amostras/iniciar-projeto/<int:amostra_id>/', iniciar_projeto, name='iniciar_projeto'),
+    path('kanban/', ProjetoKanbanView.as_view(), name='projeto_kanban'),
 ]
